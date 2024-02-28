5aabca2cef652638c47bf7e5d3676d2d14fb4de9
luukaszh <lu_kaszh@wp.pl>
2024-02-28 17:40:10 +0100

    Removed database connection data

--- main/connection.js
+++ main/connection.js
@@ -1,11 +1,11 @@
 const { Client } = require('pg')
 
 const client = new Client({
-  host: "localhost",
-  user: "postgres",
-  port: 5432,
-  password: "0000",
-  database: "postgres"
+  host: "",
+  user: "",
+  port: 0,
+  password: "",
+  database: ""
 })
 
 module.exports = client
